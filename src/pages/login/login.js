74c306113bacde41fec640141bcf1e79d09c9fbc
jmassuia <massuia1507@gmail.com>
2021-11-02 18:06:50 -0300

    Login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,7 +5,21 @@ import './styles.css';
 export default function Login(){
     return(
         <div className="login">
-            
+            <div className="container">
+                <div className="input-field">
+                    <label>Email</label>
+                    <input type="text" 
+                        placeholder="Insira seu email..."
+                    />
+                </div>
+                <div className="input-field">
+                    <label>Senha</label>
+                    <input type="password" 
+                        placeholder="Insira sua senha..."
+                    />
+                </div>
+                <button>Entrar</button>
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Switch, Route,BrowserRouter} from 'react-router-dom';
 import Home from './pages/home/home'
+import Login from './pages/login/login';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Home}/>
-                <Route path='/login'/>
+                <Route path="/" component={Home} exact/>
+                <Route path='/login' component={Login}/>
             </Switch>   
         </BrowserRouter>
     )

47b9f488f44d734e4cf0416d4dddfdf915be0e0e
jmassuia <massuia1507@gmail.com>
2021-11-02 17:17:36 -0300

    remove trash data and fix css bugs

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,21 +6,7 @@ import './styles.css'
 export default function Home(){
     return(
         <div className="home">
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            <h1>Test</h1>
-            
-            
+         
         </div>
     )
 }
